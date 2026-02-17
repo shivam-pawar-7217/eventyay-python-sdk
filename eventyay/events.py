a75c779324b71f4961b1be8d1f07873449c51158
Shivam <snshivam205@gmail.com>
2026-02-17 09:16:36 +0000

    feat:

--- eventyay/events.py
+++ eventyay/events.py
@@ -1,4 +1,5 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
+from .utils import parse_pagination_params
 
 class EventsMixin:
     """Mixin for Event-related API methods."""
@@ -19,6 +20,38 @@ class EventsMixin:
             'page_size': page_size
         }
         return self._get('events/', params=params)
+
+    def get_all_events(self) -> List[Dict[str, Any]]:
+        """
+        Fetch ALL events by automatically iterating through pages.
+        WARNING: This can take a long time for large datasets.
+        
+        Returns:
+            Complete list of all event dictionaries.
+        """
+        all_events = []
+        page = 1
+        while True:
+            response = self.get_events(page=page, page_size=50)
+            data = response.get('data', [])
+            if not data:
+                break
+            
+            all_events.extend(data)
+            
+            links = response.get('links', {})
+            next_url = links.get('next')
+            if not next_url:
+                break
+                
+            params = parse_pagination_params(next_url)
+            next_page = params.get('page') or params.get('page[number]')
+            if next_page:
+                page = int(next_page)
+            else:
+                page += 1
+                
+        return all_events
 
     def get_event(self, event_id: int) -> Dict[str, Any]:
         """
